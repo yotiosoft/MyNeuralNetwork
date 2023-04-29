ddb58f13ba17176e0548ccadbeea83e853bc3c5e
YotioSoft <ytani0323@gmail.com>
2023-04-30 02:15:31 +0900

    back_propagate()

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ def data(x1, x2):
 # input layer: x
 # hidden layer: y
 # output layer: z
-x = np.zeros(input_size)
 y = np.zeros(hidden_size)
 z = np.zeros(output_size)
 
@@ -35,13 +34,15 @@ w = np.zeros((input_size, hidden_size))     # input to hidden
 v = np.zeros((hidden_size, output_size))    # hidden to output
 
 # sample data
-samples = []
+samples_x = []
+samples_y = []
 def make_sample_data(sample_n):
     for i in range(sample_n):
         sample_x1 = random.uniform(data_min_x1, data_max_x1)
         sample_x2 = random.uniform(data_min_x2, data_max_x2)
         sample_y = data(random.uniform(data_min_x1, data_max_x1), random.uniform(data_min_x2, data_max_x2))
-        samples.append((sample_x1, sample_x2, sample_y))
+        samples_x.append([sample_x1, sample_x2])
+        samples_y.append(sample_y)
 
 def init_weights():
     for i in range(hidden_size):
@@ -52,7 +53,7 @@ def init_weights():
         for j in range(hidden_size):
             v[j, i] = random.uniform(-init_weight_range/2, init_weight_range/2)
 
-def forward_computation():
+def forward_computation(x):
     for i in range(hidden_size):
         u = 0
         for j in range(input_size):
@@ -65,15 +66,13 @@ def forward_computation():
             s += v[j, i] * y[i]
         z[i] = 1 / (1 + math.exp(-beta * s))
 
-#def back_propagate():
-    
+    return z
+
+def back_propagate():
+    for i in range(len(samples_x)):
+        output = forward_computation(samples_x[i])
+        print(output)
 
 init_weights()
-forward_computation()
-print(y)
-print(z)
-
-print(data(1.5, 2))
-
 make_sample_data(10)
-print(samples)
+back_propagate()

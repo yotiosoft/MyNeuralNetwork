82e5ba26b4b62ce6d84d3e3fa4a66d2088b8cdf7
YotioSoft <ytani0323@gmail.com>
2023-05-01 17:50:51 +0900

    make_sample_data() returns arrays

--- main.py
+++ main.py
@@ -31,26 +31,20 @@ def data(x1, x2):
 
 print(data(0, 0))
 
-# input layer: x
-# hidden layer: y
-# output layer: z
-y = np.zeros(hidden_size)
-z = np.zeros(output_size)
-
 # weights
 w = np.zeros((hidden_size, input_size))     # input to hidden
 v = np.zeros((output_size, hidden_size))    # hidden to output
 
 # sample data
-samples_x = []
-samples_y = []
 def make_sample_data(sample_n):
+    ret_x, ret_y = [], []
     for i in range(sample_n):
         sample_x1 = random.uniform(data_min_x1, data_max_x1)
         sample_x2 = random.uniform(data_min_x2, data_max_x2)
         sample_y = data(sample_x1, sample_x2)
-        samples_x.append([sample_x1, sample_x2])
-        samples_y.append(sample_y)
+        ret_x.append([sample_x1, sample_x2])
+        ret_y.append(sample_y)
+    return ret_x, ret_y
 
 def init_weights():
     for i in range(input_size):
@@ -61,28 +55,30 @@ def init_weights():
         for j in range(output_size):
             v[j, i] = random.uniform(-init_weight_range/2, init_weight_range/2)
 
-def forward_computation(x):
+Y = np.zeros(hidden_size)
+Z = np.zeros(output_size)
+def forward_computation(X):
     for i in range(input_size):
         u = 0
         for j in range(hidden_size):
-            u += w[j, i] * x[i]
-        y[j] = 1 / (1 + math.exp(-beta * u))    # u
+            u += w[j, i] * X[i]
+        Y[j] = 1 / (1 + math.exp(-beta * u))    # u
 
     for i in range(hidden_size):
         s = 0
         for j in range(output_size):
-            s += v[j, i] * y[i]
-        z[j] = 1 / (1 + math.exp(-beta * s))    # s
+            s += v[j, i] * Y[i]
+        Z[j] = 1 / (1 + math.exp(-beta * s))    # s
 
-    return z
+    return Z
 
-def back_propagate():
+def back_propagate(samples_x, samples_y):
     for n in range(len(samples_x)):
         z = forward_computation(samples_x[n])
         t = data(samples_x[n][0], samples_x[n][1])
         for j in range(hidden_size):
             for k in range(output_size):
-                v[k, j] += eta * (t[k] - z[k]) * (z[k] * (1 - z[k])) * y[j]
+                v[k, j] += eta * (t[k] - Z[k]) * (Z[k] * (1 - Z[k])) * Y[j]
     
     err_total = 0
     for i in range(len(samples_x)):
@@ -90,32 +86,34 @@ def back_propagate():
         t = data(samples_x[n][0], samples_x[n][1])
 
         for k in range(output_size):
-            err_total += t[k] - z[k]
+            err_total += t[k] - Z[k]
 
         for i in range(input_size):
             for j in range(hidden_size):
                 s = 0
                 for k in range(output_size):
-                    s += v[k, j] * (t[k] - z[k]) * (z[k] * (1 - z[k]))
-                w[j, i] += eta * s * (y[j] * (1 - y[j])) * samples_x[n][i]
+                    s += v[k, j] * (t[k] - Z[k]) * (Z[k] * (1 - Z[k]))
+                w[j, i] += eta * s * (Y[j] * (1 - Y[j])) * samples_x[n][i]
     return err_total
 
 init_weights()
-make_sample_data(1000)
+samples_x, samples_y = make_sample_data(1000)
 
+# train
 for i in range(10):
-    err_total = back_propagate()
+    err_total = back_propagate(samples_x, samples_y)
     print("v = " + str(v))
     print("w = " + str(w))
     print("err total: " + str(err_total))
+print("train done.")
+
+# test
+#test_data_x, test_data_y = make_sample_data(1000)
 
 # show figures
 plot_sample_x1 = [x[0] for x in samples_x]
 plot_sample_x2 = [x[1] for x in samples_x]
-print(plot_sample_x1[0])
-print(plot_sample_x2[0])
 plot_sample_y = [y for y in samples_y]
-print(plot_sample_y[0])
 
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(projection='3d')

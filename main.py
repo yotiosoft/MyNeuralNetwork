04a8b32a33dc18daab224c797c5fee00c4d4f253
YotioSoft <ytani0323@gmail.com>
2023-05-12 02:51:28 +0900

    (x1,x2,y) -> (x1,x2,z)

--- main.py
+++ main.py
@@ -39,14 +39,14 @@ params = sin4pi_params
 
 # sample data
 def make_sample_data(sample_n):
-    ret_x, ret_y = [], []
+    ret_x, ret_z = [], []
     for i in range(sample_n):
         sample_x1 = random.uniform(params.data_min_x1, params.data_max_x1)
         sample_x2 = random.uniform(params.data_min_x2, params.data_max_x2)
-        sample_y = params.data_func(sample_x1, sample_x2)
+        sample_z = params.data_func(sample_x1, sample_x2)
         ret_x.append([sample_x1, sample_x2])
-        ret_y.append(sample_y)
-    return ret_x, ret_y
+        ret_z.append(sample_z)
+    return ret_x, ret_z
 
 def init_weights():
     for i in range(params.input_size):
@@ -100,50 +100,58 @@ def back_propagate(training_data_x, training_data_y):
                 w[j, i] = w[j, i] + params.eta * s * (y[j] * (1 - y[j])) * training_data_x[n][i]
     return err_total
 
+def train(times, train_X, train_Z):
+    for i in range(params.train_times):
+        print("Epoch " + str(i))
+        err_total = back_propagate(train_X, train_Z)
+        print("v = " + str(v))
+        print("w = " + str(w))
+        print("err total: " + str(err_total))
+
+
+def predict(x):
+    z, _ = forward_computation(x)
+    return z
+
 # weights
 w = np.zeros((params.hidden_size, params.input_size))     # input to hidden
 v = np.zeros((params.output_size, params.hidden_size))    # hidden to output
 
 init_weights()
-samples_x, samples_y = make_sample_data(2000)
-train_x = [[1, x[0], x[1]] for x in samples_x[:1000]]
-train_y = samples_y[:1000]
-test_x = [[1, x[0], x[1]] for x in samples_x[1000:]]
-test_y = samples_y[1000:]
+samples_X, samples_Z = make_sample_data(2000)
+train_X = [[1, x[0], x[1]] for x in samples_X[:1000]]
+train_Z = samples_Z[:1000]
+test_X = [[1, x[0], x[1]] for x in samples_X[1000:]]
+test_Z = samples_Z[1000:]
 
 # train
-for i in range(params.train_times):
-    print("Epoch " + str(i))
-    err_total = back_propagate(train_x, train_y)
-    print("v = " + str(v))
-    print("w = " + str(w))
-    print("err total: " + str(err_total))
+train(params.train_times, train_X, train_Z)
 print("train done.")
 
 # test
 test_err_total = 0
 test_predicted = []
-for n in range(len(test_x)):
-    predict, _ = forward_computation(test_x[n])
-    test_predicted.append(copy.deepcopy(predict))
-    test_err_total += abs(test_y[n] - predict)
+for n in range(len(test_X)):
+    predict_result = predict(test_X[n])
+    test_predicted.append(copy.deepcopy(predict_result))
+    test_err_total += abs(test_Z[n] - predict_result)
 print("error rate: " + str(test_err_total))
 
 # show figures
-plot_train_x1 = [x[1] for x in train_x]
-plot_train_x2 = [x[2] for x in train_x]
-plot_train_y = [y for y in train_y]
+plot_train_X1 = [x[1] for x in train_X]
+plot_train_X2 = [x[2] for x in train_X]
+plot_train_Z = [y for y in train_Z]
 
-plot_test_x1 = [x[1] for x in test_x]
-plot_test_x2 = [x[2] for x in test_x]
+plot_test_X1 = [x[1] for x in test_X]
+plot_test_X2 = [x[2] for x in test_X]
 plot_test_predicted = [p[0] for p in test_predicted]
 
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(projection='3d')
-ax1.scatter(plot_train_x1, plot_train_x2, plot_train_y, color='blue')
+ax1.scatter(plot_train_X1, plot_train_X2, plot_train_Z, color='blue')
 
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(projection='3d')
-ax2.scatter(plot_test_x1, plot_test_x2, plot_test_predicted, color='green')
+ax2.scatter(plot_test_X1, plot_test_X2, plot_test_predicted, color='green')
 
 plt.show()

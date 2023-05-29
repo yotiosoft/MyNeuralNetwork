f84dff717e012afd4f0c1234ce11d50cf85cd08e
YotioSoft <ytani0323@gmail.com>
2023-05-30 00:54:25 +0900

    ticks=2

--- iris_fig.py
+++ iris_fig.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from sklearn.datasets import load_iris
 
 iris = load_iris()
@@ -7,7 +8,8 @@ x2 = iris.data[:, 2:]
 y = iris.target
 labels = iris.target_names
 
-fig, axs = plt.subplots(4, 4, figsize=(10, 7))
+fig, axs = plt.subplots(4, 4, figsize=(7, 7))
+fig.subplots_adjust(wspace=0.6, hspace=0.6)
 
 # Line 1: Sepal length
 axs[0, 0].text(0.5, 0.5, "Sepal length", horizontalalignment="center", verticalalignment="center", transform=axs[0, 0].transAxes)
@@ -16,19 +18,23 @@ axs[0, 0].axis("off")
 axs[0, 1].scatter(x1[:, 1], x1[:, 0], c=y, cmap=plt.cm.Set1, s=5)
 axs[0, 1].set_xlabel("Sepal width")
 axs[0, 1].set_ylabel("Sepal length")
+axs[0, 1].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 axs[0, 2].scatter(x2[:, 0], x1[:, 0], c=y, cmap=plt.cm.Set1, s=5)
 axs[0, 2].set_xlabel("Petal length")
 axs[0, 2].set_ylabel("Sepal length")
+axs[0, 2].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 axs[0, 3].scatter(x2[:, 1], x1[:, 0], c=y, cmap=plt.cm.Set1, s=5)
 axs[0, 3].set_xlabel("Petal width")
 axs[0, 3].set_ylabel("Sepal length")
+axs[0, 3].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 # Line 2: Sepal width
 axs[1, 0].scatter(x1[:, 0], x1[:, 1], c=y, cmap=plt.cm.Set1, s=5)
 axs[1, 0].set_xlabel("Sepal length")
 axs[1, 0].set_ylabel("Sepal width")
+axs[1, 0].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 axs[1, 1].text(0.5, 0.5, "Sepal width", horizontalalignment="center", verticalalignment="center", transform=axs[1, 1].transAxes)
 axs[1, 1].axis("off")
@@ -36,19 +42,23 @@ axs[1, 1].axis("off")
 axs[1, 2].scatter(x2[:, 0], x1[:, 1], c=y, cmap=plt.cm.Set1, s=5)
 axs[1, 2].set_xlabel("Petal length")
 axs[1, 2].set_ylabel("Sepal width")
+axs[1, 2].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 axs[1, 3].scatter(x2[:, 1], x1[:, 1], c=y, cmap=plt.cm.Set1, s=5)
 axs[1, 3].set_xlabel("Petal width")
 axs[1, 3].set_ylabel("Sepal width")
+axs[1, 3].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 # Line 3: Petal length
 axs[2, 0].scatter(x1[:, 0], x2[:, 0], c=y, cmap=plt.cm.Set1, s=5)
 axs[2, 0].set_xlabel("Sepal length")
 axs[2, 0].set_ylabel("Petal length")
+axs[2, 0].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 axs[2, 1].scatter(x1[:, 1], x2[:, 0], c=y, cmap=plt.cm.Set1, s=5)
 axs[2, 1].set_xlabel("Sepal width")
 axs[2, 1].set_ylabel("Petal length")
+axs[2, 1].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 axs[2, 2].text(0.5, 0.5, "Petal length", horizontalalignment="center", verticalalignment="center", transform=axs[2, 2].transAxes)
 axs[2, 2].axis("off")
@@ -56,19 +66,23 @@ axs[2, 2].axis("off")
 axs[2, 3].scatter(x2[:, 1], x2[:, 0], c=y, cmap=plt.cm.Set1, s=5)
 axs[2, 3].set_xlabel("Petal width")
 axs[2, 3].set_ylabel("Petal length")
+axs[2, 3].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 # Line 4: Petal width
 axs[3, 0].scatter(x1[:, 0], x2[:, 1], c=y, cmap=plt.cm.Set1, s=5)
 axs[3, 0].set_xlabel("Sepal length")
 axs[3, 0].set_ylabel("Petal width")
+axs[3, 0].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 axs[3, 1].scatter(x1[:, 1], x2[:, 1], c=y, cmap=plt.cm.Set1, s=5)
 axs[3, 1].set_xlabel("Sepal width")
 axs[3, 1].set_ylabel("Petal width")
+axs[3, 1].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 axs[3, 2].scatter(x2[:, 0], x2[:, 1], c=y, cmap=plt.cm.Set1, s=5)
 axs[3, 2].set_xlabel("Petal length")
 axs[3, 2].set_ylabel("Petal width")
+axs[3, 2].yaxis.set_major_locator(ticker.MultipleLocator(2))
 
 axs[3, 3].text(0.5, 0.5, "Petal width", horizontalalignment="center", verticalalignment="center", transform=axs[3, 3].transAxes)
 axs[3, 3].axis("off")

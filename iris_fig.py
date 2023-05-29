87f50ee3cea2be49917d9219a67cf634ff4e808f
YotioSoft <ytani0323@gmail.com>
2023-05-29 13:20:47 +0900

    hmm

--- iris_fig.py
+++ iris_fig.py
@@ -5,10 +5,14 @@ iris = load_iris()
 x1 = iris.data[:, :2]
 x2 = iris.data[:, 2:]
 y = iris.target
+labels = iris.target_names
 
-fig, axs = plt.subplots(4, 4, figsize=(7, 7))
+fig, axs = plt.subplots(4, 4, figsize=(10, 7))
 
 # Line 1: Sepal length
+axs[0, 0].text(0.5, 0.5, "Sepal length", horizontalalignment="center", verticalalignment="center", transform=axs[0, 0].transAxes)
+axs[0, 0].axis("off")
+
 axs[0, 1].scatter(x1[:, 1], x1[:, 0], c=y, cmap=plt.cm.Set1, s=5)
 axs[0, 1].set_xlabel("Sepal width")
 axs[0, 1].set_ylabel("Sepal length")
@@ -26,8 +30,10 @@ axs[1, 0].scatter(x1[:, 0], x1[:, 1], c=y, cmap=plt.cm.Set1, s=5)
 axs[1, 0].set_xlabel("Sepal length")
 axs[1, 0].set_ylabel("Sepal width")
 
+axs[1, 1].text(0.5, 0.5, "Sepal width", horizontalalignment="center", verticalalignment="center", transform=axs[1, 1].transAxes)
+axs[1, 1].axis("off")
+
 axs[1, 2].scatter(x2[:, 0], x1[:, 1], c=y, cmap=plt.cm.Set1, s=5)
-axs[1, 2].set_title("Sepal width x Petal length")
 axs[1, 2].set_xlabel("Petal length")
 axs[1, 2].set_ylabel("Sepal width")
 
@@ -43,6 +49,9 @@ axs[2, 0].set_ylabel("Petal length")
 axs[2, 1].scatter(x1[:, 1], x2[:, 0], c=y, cmap=plt.cm.Set1, s=5)
 axs[2, 1].set_xlabel("Sepal width")
 axs[2, 1].set_ylabel("Petal length")
+
+axs[2, 2].text(0.5, 0.5, "Petal length", horizontalalignment="center", verticalalignment="center", transform=axs[2, 2].transAxes)
+axs[2, 2].axis("off")
 
 axs[2, 3].scatter(x2[:, 1], x2[:, 0], c=y, cmap=plt.cm.Set1, s=5)
 axs[2, 3].set_xlabel("Petal width")
@@ -61,6 +70,9 @@ axs[3, 2].scatter(x2[:, 0], x2[:, 1], c=y, cmap=plt.cm.Set1, s=5)
 axs[3, 2].set_xlabel("Petal length")
 axs[3, 2].set_ylabel("Petal width")
 
-fig.tight_layout()
+axs[3, 3].text(0.5, 0.5, "Petal width", horizontalalignment="center", verticalalignment="center", transform=axs[3, 3].transAxes)
+axs[3, 3].axis("off")
+
+axs[0, 3].legend(["setosa", "versicolor", "virginica"], loc="upper left", bbox_to_anchor=(1.05, 1.0), borderaxespad=0)
 
 plt.show()
